37776b962fc23241f6db1e6d22c8b61de5ed8aa6
wmingjian <wmingjian@12f60578-7ab0-11de-98bd-fdc22f85cdda>
2011-03-22 15:34:45 +0000

    +demos,docs应用 +WebRuntime::regApp +alz.mui.TableView(未完成) 分离版权信息到copyright.js

--- build/_inc.php
+++ build/_inc.php
@@ -1,5 +1,6 @@
 <?php
 $install_dir = 'alzui-X201';
 $path_root = $_SERVER['DOCUMENT_ROOT'];
+$path_upload = $path_root . '/' . $install_dir . '/upload/';
 #$path_root = '/www/alzui-dev';
 $autoGenFile = false;  //是否自动生成lib,tpl文件
---
+++ docs/src/lib/docs.tpl.js
@@ -0,0 +1,6 @@
+/*
+ * alzui-mini 文档中心模板库
+ */
+runtime.regTpl("docs", {
+pane_main.xml
+});
