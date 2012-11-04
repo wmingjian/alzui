a0103b54b12c37dba5c6e95d36acd751988f35c1
wmingjian <wmingjian@12f60578-7ab0-11de-98bd-fdc22f85cdda>
2012-11-04 09:21:25 +0000

    整理文档格式

--- trunk/docs/lib/foot.js
+++ trunk/docs/lib/foot.js
@@ -1,5 +1,1 @@
-/* 公共页尾 */
-document.write("<div class=\"foot\">"
-	+ "<hr />"
-	+ "<p>Copyright &copy; 2009 Sina Inc. All Right Reserved. </p>"
-	+ "</div>");
+chm.foot();
--- trunk/docs/lib/header.js
+++
@@ -1,2 +0,0 @@
-document.write('<h1 class="title">' + document.title + '</h1>');
-document.write('<hr size="2" noshade="noshade" />');
